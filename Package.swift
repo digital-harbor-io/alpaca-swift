3cc612666cc042115628a2803d51c45a6ae8a7cd
Mike <mjKosmic@gmail.com>
2023-04-20 23:19:05 -0500

    added some more directives to Package.swift

--- Package.swift
+++ Package.swift
@@ -3,11 +3,15 @@ import PackageDescription
 
 let package = Package(
 	name: "Alpaca",
+    platforms: [
+        .iOS("15.0")
+    ],
     products: [
         .library(name: "Alpaca", targets: ["Alpaca"])
     ],
+    dependencies: [],
 	targets: [
-		.target(name: "Alpaca"),
+		.target(name: "Alpaca", dependencies: []),
 		.testTarget(name: "Tests", dependencies: ["Alpaca"])
 	]
 )
